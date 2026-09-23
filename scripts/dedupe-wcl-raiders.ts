0cf1bfc6a2012ac1d2d9b80382cdc84fe916fe86
Josh M <teng3n@gmail.com>
2026-09-22 20:07:56 -0700

    fix: merge owner alts into Tengen identity

--- scripts/dedupe-wcl-raiders.ts
+++ scripts/dedupe-wcl-raiders.ts
@@ -85,7 +85,7 @@ mergeNames(["Leafea", "Leafealol"]);
 mergeNames(["Lilgargussy", "Lilmcmussy"]);
 mergeNames(["Mavadin", "Mavaman", "Mavdeath", "Mavdog", "Maverickdog", "Maverlock"]);
 mergeNames(["Popsicles", "Popsiclës", "Pøpsicles"]);
-mergeNames(["Tengen", "Tengenn", "Tenngenn", "Tenaman"]);
+mergeNames(["Tengen", "Tengenn", "Tenngenn", "Tenaman", "Magegen", "Deathgen"]);
 mergeMatches((name) => visualKey(name).startsWith("stim"));
 mergeMatches((name) => /^sti+ngr/u.test(visualKey(name)) || ["stiing", "stingg"].includes(visualKey(name)));
 
